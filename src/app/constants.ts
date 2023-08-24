d5f27f70e492d952aa22b2ad637d7d22a324be2e
Andrea Cingolani <cingolani.andrea@hotmail.it>
2023-08-24 18:05:37 +0200

    environments - constant - dashboard

--- src/app/constants.ts
+++
@@ -1,5 +0,0 @@
-export class Constants{
-    public static ITALY_ID = 135;
-    public static SERIE_A_ID = 135;
-    public static YEAR_CURRENT_SEASON = 2023;
-}
--- src/app/services/fanta-api.service.ts
+++ src/app/services/fanta-api.service.ts
@@ -1,27 +1,21 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Constants } from '../constants';
+import { Constants } from '../../common/service-constants';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class FantaApiService {
-  api: string = `https://api-football-v1.p.rapidapi.com/v3`;
-
   constructor(private http: HttpClient) {}
 
   players(): void {
-    const options = {
-      headers: {
-        'X-RapidAPI-Key': '75feb123ddmshd808329cbfb1fb1p123e11jsn68636127a384',
-        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-      }
-    };
-
-    this.http.get<any>(`${this.api}/players?league=${Constants.SERIE_A_ID}&season=${Constants.YEAR_CURRENT_SEASON}&search=Immobile`, options).subscribe(res =>{
-      console.log(res);
-    });
-
+    this.http
+      .get<any>(
+        `${Constants.API_ENDPOINT}/players?league=${Constants.SERIE_A_ID}&season=${Constants.YEAR_CURRENT_SEASON}&search=Immobile`,
+        Constants.OPTIONS
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 }
