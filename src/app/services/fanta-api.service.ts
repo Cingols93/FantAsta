9253fb1c3633619aa2554ec52f6186ff2ff7e092
Andrea Cingolani <cingolani.andrea@hotmail.it>
2023-08-24 18:45:52 +0200

    fix api + routing

--- src/app/services/fanta-api.service.ts
+++ src/app/services/fanta-api.service.ts
@@ -1,21 +1,22 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Constants } from '../../common/service-constants';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FantaApiService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private apiService: ApiService) {}
 
   players(): void {
-    this.http
-      .get<any>(
-        `${Constants.API_ENDPOINT}/players?league=${Constants.SERIE_A_ID}&season=${Constants.YEAR_CURRENT_SEASON}&search=Immobile`,
-        Constants.OPTIONS
-      )
-      .subscribe((res) => {
-        console.log(res);
-      });
+    let params = {
+      team: Constants.SERIE_A_ID,
+      season: Constants.YEAR_CURRENT_SEASON,
+    };
+    
+    this.apiService.get('/players', params).subscribe((res) => {
+      console.log(res);
+    });
   }
 }
