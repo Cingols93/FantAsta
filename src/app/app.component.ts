2a9e221ee2aa971451d09794c1f19ea665ed34bc
Andrea Cingolani <cingolani.andrea@hotmail.it>
2023-08-24 17:39:38 +0200

    test fanta-api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FantaApiService } from './services/fanta-api.service';
 
 @Component({
   selector: 'app-root',
-  template: `
-    
-  `,
-  styleUrls: ['./app.component.scss']
+  template: ``,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'FantAsta';
+
+  constructor(private fantaApiService: FantaApiService) {
+    this.fantaApiService.players();
+  }
 }
