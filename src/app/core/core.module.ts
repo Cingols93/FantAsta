f51461b17786934526ce1062ad5fd22a3247ff8d
StefanoMRC <stefano.mrc92@gmail.com>
2023-08-29 13:03:11 +0200

    aggiunta pagina registration e collegamento router

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,10 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
-  declarations: [HeaderComponent, LoginComponent, SidenavComponent],
+  declarations: [HeaderComponent, LoginComponent, SidenavComponent, RegistrationComponent],
   imports: [CommonModule,RouterModule],
   exports: [HeaderComponent, LoginComponent, SidenavComponent],
 })
